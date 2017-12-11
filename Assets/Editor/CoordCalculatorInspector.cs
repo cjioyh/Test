1e8c88c971201eb7a349864a02a9eb14780a2da5
cjioyh <cjioyh@gmail.com>
2017-12-11 15:06:58 +0200

    //

---
+++ Assets/Editor/CoordCalculatorInspector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CoordCalculator))]
+public class CoordCalculatorInspector : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        var tr = (CoordCalculator)target;
+        var flag = EditorGUILayout.Toggle("Track Pos", tr.savePos);
+        if (flag)
+        {
+            EditorGUILayout.Vector3Field("Delta Pos", tr.gameObject.transform.localPosition - (tr.gameObject.transform.position - tr.pos));
+        }
+        if (flag != tr.savePos && !tr.savePos && flag)
+        {
+            tr.pos = tr.gameObject.transform.position;
+        }
+        tr.savePos = flag;
+        EditorGUILayout.EndVertical();
+
+    }
+}
+
---
+++ Assets/scripts/CoordCalculator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CoordCalculator : MonoBehaviour
+{
+    [SerializeField]
+    public bool savePos;
+
+    [SerializeField]
+    public Vector3 pos;
+}
